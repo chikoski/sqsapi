df5109b58a5cacdd38aa79938d05a17eb6094e27
N. Shimizu <chikoski@gmail.com>
2013-02-28 16:26:28 +0900

    decorated public/translate.html with Twitter Bootstrap and sample SQS files

--- lib/sqs/api/config.rb
+++ lib/sqs/api/config.rb
@@ -26,7 +26,7 @@ class SQS::API::Config
 
     @yml["fixture_dir"] ||= @yml["fixture_dir"] || dir[:fixtures] || dir["fixtures"]
     @yml["fixture_dir"] =
-      File.expand_path(@yml[:fixture_dir], @yml[:root_dir])
+      File.expand_path(@yml["fixture_dir"], @yml[:root_dir])
     @yml["public_dir"] = File.expand_path("public", @yml[:root_dir])
 
     @yml[:m2] = dir[:m2] || dir["m2"] ||  File.expand_path(".m2", ENV["HOME"])
